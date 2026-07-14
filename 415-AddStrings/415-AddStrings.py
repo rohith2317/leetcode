00968746ae7ae1e923826d1ba26c01db1d20437d
ROHITH <rohithbp23@gmail.com>
2026-07-14 11:22:57 +0530

    Create 415-AddStrings

---
+++ 415-AddStrings/415-AddStrings.py
@@ -0,0 +1,9 @@
+# Last updated: 14/07/2026, 11:22:54
+class Solution(object):
+    def addStrings(self, num1, num2):
+        num1=int(num1)
+        num2=int(num2)
+        num=num1+num2
+        result=str(num)
+        return result
+        
