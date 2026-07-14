df508856659735d14993efcc4e4d788db59cad5e
ROHITH <rohithbp23@gmail.com>
2026-07-14 11:22:59 +0530

    Create 414-ThirdMaximumNumber

---
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 14/07/2026, 11:22:56
+import java.util.*;
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        for (int n : nums) s.add(n);
+        
+        if (s.size() < 3) return Collections.max(s);
+        
+        s.remove(Collections.max(s));
+        s.remove(Collections.max(s));
+        
+        return Collections.max(s);
+    }
+}
