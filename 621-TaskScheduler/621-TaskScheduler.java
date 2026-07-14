cb163131250c522cc3ca7f6025bebe49ce4d65e5
ROHITH <rohithbp23@gmail.com>
2026-07-14 11:22:52 +0530

    Create 621-TaskScheduler

---
+++ 621-TaskScheduler/621-TaskScheduler.java
@@ -0,0 +1,18 @@
+// Last updated: 14/07/2026, 11:22:48
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+        for (char task : tasks) {
+            freq[task - 'A']++;
+        }
+        Arrays.sort(freq);
+        int chunk = freq[25] - 1;
+        int idle = chunk * n;
+
+        for (int i = 24; i >= 0; i--) {
+            idle -= Math.min(chunk, freq[i]);
+        }
+
+        return idle < 0 ? tasks.length : tasks.length + idle;
+    }
+}
