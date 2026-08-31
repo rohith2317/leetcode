8486f6526be8812d14a445e4c4a99aa0a6a8413a
ROHITH <rohithbp23@gmail.com>
2026-08-31 15:01:51 +0530

    Create 327-CountofRangeSum

---
+++ 327-CountofRangeSum/327-CountofRangeSum.py
@@ -0,0 +1,13 @@
+# Last updated: 31/08/2026, 15:01:51
+1class Solution:
+2    def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
+3        psum = [0]
+4        for num in nums:
+5            psum.append(psum[-1] + num)
+6
+7        result, slist = 0, SortedList()
+8        for pval in reversed(psum):
+9            result += slist.bisect_right(pval + upper) - slist.bisect_left(pval + lower)
+10            slist.add(pval)
+11
+12        return result
