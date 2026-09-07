3bcec69dd48bd17345da5bf15d2f37008ad3c35c
ROHITH <rohithbp23@gmail.com>
2026-09-07 14:35:22 +0530

    File updated at 07/09/2026, 14:35:20

--- 2190-MostFrequentNumberFollowingKeyInanArray/2190-MostFrequentNumberFollowingKeyInanArray.java
+++ 2190-MostFrequentNumberFollowingKeyInanArray/2190-MostFrequentNumberFollowingKeyInanArray.java
@@ -1,27 +1,23 @@
-// Last updated: 07/09/2026, 14:34:53
+// Last updated: 07/09/2026, 14:35:20
 1class Solution {
 2    public int mostFrequent(int[] nums, int key) {
 3
 4        int[] count = new int[1001];
 5
 6        for (int i = 0; i < nums.length - 1; i++) {
-7
-8            if (nums[i] == key) {
-9                count[nums[i + 1]]++;
-10            }
-11        }
-12
-13        int answer = 0;
-14        int max = 0;
-15
-16        for (int i = 0; i < count.length; i++) {
-17
-18            if (count[i] > max) {
-19                max = count[i];
-20                answer = i;
-21            }
-22        }
-23
-24        return answer;
-25    }
-26}
+7            if (nums[i] == key) {
+8                count[nums[i + 1]]++;
+9            }
+10        }
+11        int answer = 0;
+12        int max = 0;
+13        for (int i = 0; i < count.length; i++) {
+14            if (count[i] > max) {
+15                max = count[i];
+16                answer = i;
+17            }
+18        }
+19
+20        return answer;
+21    }
+22}
