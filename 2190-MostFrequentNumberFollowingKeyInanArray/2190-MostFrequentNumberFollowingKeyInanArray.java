090a6d63361dcf0686446884fb4c122587e73bf4
ROHITH <rohithbp23@gmail.com>
2026-09-07 14:34:55 +0530

    File updated at 07/09/2026, 14:34:53

--- 2190-MostFrequentNumberFollowingKeyInanArray/2190-MostFrequentNumberFollowingKeyInanArray.java
+++ 2190-MostFrequentNumberFollowingKeyInanArray/2190-MostFrequentNumberFollowingKeyInanArray.java
@@ -1,22 +1,27 @@
-// Last updated: 07/09/2026, 14:31:46
+// Last updated: 07/09/2026, 14:34:53
 1class Solution {
 2    public int mostFrequent(int[] nums, int key) {
-3        Map<Integer, Integer> map = new HashMap<>();
-4        int maxCount = 0;
-5        int result = 0;
-6
-7        for (int i = 0; i < nums.length - 1; i++) {
+3
+4        int[] count = new int[1001];
+5
+6        for (int i = 0; i < nums.length - 1; i++) {
+7
 8            if (nums[i] == key) {
-9                int next = nums[i + 1];
-10                map.put(next, map.getOrDefault(next, 0) + 1);
-11
-12                if (map.get(next) > maxCount) {
-13                    maxCount = map.get(next);
-14                    result = next;
-15                }
-16            }
-17        }
-18
-19        return result;
-20    }
-21}
+9                count[nums[i + 1]]++;
+10            }
+11        }
+12
+13        int answer = 0;
+14        int max = 0;
+15
+16        for (int i = 0; i < count.length; i++) {
+17
+18            if (count[i] > max) {
+19                max = count[i];
+20                answer = i;
+21            }
+22        }
+23
+24        return answer;
+25    }
+26}
