cabb9b12aff5c6e15dec7fdead883d8b964d80a7
ROHITH <rohithbp23@gmail.com>
2026-09-07 14:41:37 +0530

    File updated at 07/09/2026, 14:41:35

--- 2190-MostFrequentNumberFollowingKeyInanArray/2190-MostFrequentNumberFollowingKeyInanArray.java
+++ 2190-MostFrequentNumberFollowingKeyInanArray/2190-MostFrequentNumberFollowingKeyInanArray.java
@@ -1,23 +1,27 @@
-// Last updated: 07/09/2026, 14:35:20
+// Last updated: 07/09/2026, 14:41:35
 1class Solution {
-2    public int mostFrequent(int[] nums, int key) {
-3
-4        int[] count = new int[1001];
-5
-6        for (int i = 0; i < nums.length - 1; i++) {
-7            if (nums[i] == key) {
-8                count[nums[i + 1]]++;
-9            }
-10        }
-11        int answer = 0;
-12        int max = 0;
-13        for (int i = 0; i < count.length; i++) {
-14            if (count[i] > max) {
-15                max = count[i];
-16                answer = i;
-17            }
-18        }
-19
-20        return answer;
-21    }
-22}
+2    public int splitArray(int[] nums, int k) {
+3        int left =0, right =0;
+4        for(int num : nums){
+5            left = Math.max(left, num); 
+6            right += num; 
+7        }
+8        while(left<=right){
+9            int mid = left + (right - left)/2;
+10            int parts = 1;
+11            int currentSum = 0;
+12
+13            for(int num : nums){
+14                if(currentSum + num > mid){
+15                    parts++;
+16                    currentSum = num;
+17                }else{
+18                    currentSum += num;
+19                }
+20            }
+21            if(parts<=k) right = mid - 1;
+22            else left = mid + 1; 
+23               }
+24        return left;
+25    }
+26}
